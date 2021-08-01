ce7031b5f74f3665682a3afe55b3f6b226e77a01
Matias <matias.wolcraft3@gmail.com>
2021-07-31 23:53:32 -0400

    State de alerta creada

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,7 +1,14 @@
-import React,{useState} from 'react';
+import React,{useState,useContext} from 'react';
 import { Link } from 'react-router-dom';
+import AlertaContext from '../../context/alertas/alertaContext';
+
 
 const NuevaCuenta = () => {
+
+
+    //extraer los valores del context
+    const alertaContext = useContext(AlertaContext);
+    const {alerta,mostrarAlerta} =alertaContext;
     
     //State para iniciar sesion
     const [usuario, guardarUsuario] = useState({
@@ -26,15 +33,29 @@ const NuevaCuenta = () => {
 
     //Cuando el usuario quiere iniciar sesion
     const onsubmit = e =>{
-        e.preventDefault()
+        e.preventDefault();
+        
 
         //Validar que no haya campos vacios
-
+        if(nombre.trim() === ''|| 
+            email.trim() === ''||
+            password.trim() === ''||
+            confirmar.trim() === ''){
+                mostrarAlerta('Todos los campos son obligatorios','alerta-error');
+                return;
+            }
 
         //Password minimo de 6 caracteres
-
+        if(password.length < 6){
+            mostrarAlerta('El password debe ser de al menos 6 caracteres','alerta-error');
+            return;
+        }
 
         //Los 2 password son iguales
+        if(password !==confirmar){
+            mostrarAlerta('Los password deben ser iguales','alerta-error');
+            return;
+        }
 
 
         //Pasar al action
@@ -45,10 +66,11 @@ const NuevaCuenta = () => {
 
         return ( 
             <div className="form-usuario"> 
+                {alerta ?(<div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>) :null}
                 <div className="contenedor-form sombra-dark">
                     <h1>Crear Cuenta</h1>
                     <form
-                        onsubmit={onsubmit}
+                        onSubmit={onsubmit}
                     >
                         <div className="campo-form">
                             <label htmlFor="nombre">Nombre</label>
